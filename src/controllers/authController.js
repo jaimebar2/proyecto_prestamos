19e3cc6f588400163e51062e2729a0a1886093f0
Jaime Barrios <jaimeyapia2601@gmail.com>
2026-05-23 17:20:05 -0500

    backend listo

---
+++ src/controllers/authController.js
@@ -0,0 +1,70 @@
+const pool = require("../config/db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const login = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { correo, password } = req.body;
+
+    const result = await pool.query(
+      "SELECT * FROM usuarios WHERE correo = $1",
+      [correo]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        mensaje: "Usuario no encontrado",
+      });
+    }
+
+    const usuario = result.rows[0];
+
+    console.log(usuario.password);
+
+    const validPassword = await bcrypt.compare(
+      password,
+      usuario.password
+    );
+
+    console.log(validPassword);
+
+    if (!validPassword) {
+      return res.status(401).json({
+        mensaje: "Contraseña incorrecta",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: usuario.id,
+        correo: usuario.correo,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "8h",
+      }
+    );
+
+    res.json({
+      token,
+      usuario: {
+        id: usuario.id,
+        nombre: usuario.nombre,
+        correo: usuario.correo,
+      },
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      mensaje: "Error del servidor",
+    });
+  }
+};
+
+module.exports = {
+  login,
+};
